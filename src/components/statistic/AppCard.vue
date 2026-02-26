95db0b74ca598316d318a6eb79959841537699cd
saicem <saicem@qq.com>
2026-02-26 16:56:34 +0800

    feat: timeline use more compact layout

--- src/components/statistic/AppCard.vue
+++ src/components/statistic/AppCard.vue
@@ -12,31 +12,39 @@ defineProps<{
   <el-card>
     <div
       style="
-        height: 130px;
-        width: 130px;
-        flex: auto;
+        height: 36px;
+        width: 150px;
         display: flex;
-        flex-direction: column;
+        flex-direction: row;
         align-items: center;
         gap: 8px;
       "
     >
-      <div>
-        <app-icon :icon="detail.icon" :size="64" />
-      </div>
-      <p
+      <app-icon style="flex: 0 0 auto" :icon="detail.icon" :size="32" />
+      <div
         style="
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-          width: 100%;
-          font-weight: bold;
-          text-align: center;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
         "
       >
-        {{ detail?.name }}
-      </p>
-      <p style="color: var(--accent-color-1)">{{ formatDuration(duration) }}</p>
+        <p
+          style="
+            font-weight: bold;
+            text-align: center;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            width: 110px;
+          "
+        >
+          {{ detail?.name }}
+        </p>
+        <p style="color: var(--accent-color-1); text-align: center">
+          {{ formatDuration(duration) }}
+        </p>
+      </div>
     </div>
   </el-card>
 </template>
