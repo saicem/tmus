b8e8b60195f9483dc2651d0743dced88bc9d8ba7
saicem <saicem@qq.com>
2026-02-26 21:09:08 +0800

    feat: home polar chart use smaller granularity

--- src/components/statistic/WeeklyPolar.vue
+++ src/components/statistic/WeeklyPolar.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { Chart } from "@antv/g2"
 import {
-  MILLISECONDS_PER_HOUR,
+  MILLISECONDS_PER_DAY,
   MILLISECONDS_PER_MINUTE,
 } from "@/script/time-util.ts"
 import { startOfDay, subDays } from "date-fns"
@@ -9,70 +9,92 @@ import { queryDurationStatistic } from "@/script/cmd.ts"
 import { i18n } from "@/script/i18n.ts"
 import { configStore } from "@/script/state.ts"
 
-const data = ref<{ hour: string; week: string; duration: number }[]>([])
+interface ChartData {
+  time: number
+  tag: string
+  duration: number
+  tooltipTitle: string
+}
+
+const data = ref<ChartData[]>([])
 const chartContainer = ref<HTMLDivElement | null>(null)
+
+const granularityInMinute = 10
+const granularity = MILLISECONDS_PER_MINUTE * granularityInMinute
+const cycle = MILLISECONDS_PER_DAY / granularity
+const cycleInHour = cycle / 24
 
 async function loadData() {
   let now = new Date()
   let pastWeekEnd = startOfDay(now)
   let pastWeekStart = subDays(pastWeekEnd, 7)
 
-  let newData: {
-    hour: string
-    week: string
-    duration: number
-  }[] = []
-  // The past week average
+  let newData: ChartData[] = []
+  // The past week average, the statistical granularity is 10 minutes
   const pastWeekData = toMap(
     await queryDurationStatistic(
       pastWeekStart.getTime(),
       pastWeekEnd.getTime(),
       true,
       null,
-      MILLISECONDS_PER_HOUR,
-      24
+      granularity,
+      cycle
     ),
     (x) => [
-      x.intervalStart / MILLISECONDS_PER_HOUR,
-      +(x.duration / (MILLISECONDS_PER_MINUTE * 7)).toFixed(1),
+      x.intervalStart / granularity,
+      +(x.duration / MILLISECONDS_PER_MINUTE / 7).toFixed(1),
     ]
   )
   newData.push(
-    ...range(0, 24).map((x) => {
+    ...range(0, cycle).map((x) => {
       return {
-        hour: x.toString(),
-        week: i18n.value.homePage.pastWeekAverage,
+        time: x / cycleInHour,
+        tag: i18n.value.homePage.pastWeekAverage,
         duration: pastWeekData.get(x) ?? 0,
+        tooltipTitle: computeTooltipTitle(x),
       }
     })
   )
 
-  // Today
+  // Today, as above
   const todayData = toMap(
     await queryDurationStatistic(
       pastWeekEnd.getTime(),
       now.getTime(),
       true,
       null,
-      MILLISECONDS_PER_HOUR,
-      24
+      granularity,
+      cycle
     ),
     (x) => [
-      x.intervalStart / MILLISECONDS_PER_HOUR,
+      x.intervalStart / granularity,
       +(x.duration / MILLISECONDS_PER_MINUTE).toFixed(1),
     ]
   )
-  console.log("todayData", todayData, pastWeekData)
   newData.push(
-    ...range(0, 24).map((x) => {
+    ...range(0, cycle).map((x) => {
       return {
-        hour: x.toString(),
-        week: i18n.value.common.today,
+        time: x / cycleInHour,
+        tag: i18n.value.common.today,
         duration: todayData.get(x) ?? 0,
+        tooltipTitle: computeTooltipTitle(x),
       }
     })
   )
   data.value = newData
+}
+
+function computeTooltipTitle(cycleNo: number) {
+  return `${computeTooltipTitleItem(cycleNo)} - ${computeTooltipTitleItem(
+    cycleNo + 1
+  )}`
+}
+
+function computeTooltipTitleItem(cycleNo: number) {
+  const cycleInHourNo = cycleNo % cycleInHour
+  const h = (cycleNo - cycleInHourNo) / cycleInHour
+  const m = cycleInHourNo * granularityInMinute
+  return `${h}:${m.toString().padStart(2, "0")}`
 }
 
 function renderChart(container: HTMLElement) {
@@ -82,13 +104,20 @@ function renderChart(container: HTMLElement) {
     height: 320,
   })
 
-  chart.coordinate({ type: "polar" })
-
   chart
+    .coordinate({ type: "polar" })
     .data(data.value)
-    .scale("x", { padding: 0.5, align: 0 })
-    .scale("y", { domain: [0, 60] })
-    .axis("x", { grid: true })
+    .scale("x", {
+      padding: 0.5,
+      align: 0,
+      tickCount: 24,
+      domain: [0, 24],
+    })
+    .scale("y", { domain: [0, granularityInMinute] })
+    // Fuck! Is customizing the coordinate axis really this complicated?
+    .axis("x", {
+      tickFilter: (_: any, i: number) => i !== 24,
+    })
     .axis("y", {
       zIndex: 1,
       title: false,
@@ -97,24 +126,36 @@ function renderChart(container: HTMLElement) {
 
   chart
     .area()
-    .encode("x", "hour")
+    .encode("x", "time")
     .encode("y", "duration")
-    .encode("color", "week")
+    .encode("color", "tag")
     .encode("shape", "smooth")
     .style("fillOpacity", 0.5)
-    .scale("x", { domainMax: 23 })
-    .tooltip(null)
+    .tooltip({
+      title: (x: ChartData) => x.tooltipTitle,
+      items: [
+        {
+          channel: "y",
+          valueFormatter: (x) => `${x}m`,
+        },
+      ],
+    })
 
   chart
     .line()
-    .encode("x", "hour")
+    .encode("x", "time")
     .encode("y", "duration")
-    .encode("color", "week")
+    .encode("color", "tag")
     .encode("shape", "smooth")
-    .style("lineWidth", 2)
+    .style("lineWidth", 1)
     .tooltip(null)
 
-  chart.interaction("tooltip", { crosshairsLineDash: [4, 4] })
+  chart.interaction("tooltip", {
+    crosshairsLineDash: [4, 4],
+    filter: (d: any) => {
+      return d.value != "0m"
+    },
+  })
 
   chart.render()
 }
@@ -122,7 +163,6 @@ function renderChart(container: HTMLElement) {
 onMounted(async () => {
   if (chartContainer?.value) {
     await loadData()
-    console.log(data.value)
     renderChart(chartContainer.value)
   }
 })
