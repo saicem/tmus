3ee5464f892a67827ff996bd28022a1af339861d
saicem <saicem@qq.com>
2026-03-05 14:34:24 +0800

    chore: update dependency

--- components.d.ts
+++ components.d.ts
@@ -29,7 +29,6 @@ declare module 'vue' {
     ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
-    ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
